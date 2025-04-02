e15e46f60703e1f3dd4dc1355e531a175f52ce5f
Andrew <103331877+Allials@users.noreply.github.com>
2025-04-02 18:50:27 -0500

    Set up dependencies with bg color and font

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # url-shortener
+
 URL shortener to that takes a long url then returns a shorter link
---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import Header from './components/Header';
+
+
+export default function App() {
+  return (
+    <>
+     <div className="min-w-[535px] max-w-screen-sm mx-auto">
+        <Header />
+      </div>       
+    </>
+  )
+}
+
